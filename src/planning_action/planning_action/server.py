38194442ed7b4184f0e453055fc977021b93ae0c
ufukguenes <u.guenes@live.de>
2025-11-21 17:07:58 +0100

    trying to get stuff to work

--- src/planning_action/planning_action/server.py
+++ src/planning_action/planning_action/server.py
@@ -9,12 +9,24 @@ import os
 import time
 from openai import OpenAI
 from dotenv import load_dotenv, dotenv_values
+from rclpy.action import ActionClient
+from moveit.planning import PlanningComponent, MoveItPy
+
+from rclpy.executors import MultiThreadedExecutor
+from moveit.planning import MoveItPy
+from moveit_configs_utils import MoveItConfigsBuilder
 
 
 class PlanningActionServer(Node):
 
-    def __init__(self):
+    def __init__(self, moveit_node):
         super().__init__("planning_action_server")
+
+        self.moveit_node = moveit_node
+
+        self.planning_component = self.moveit_node.get_planning_component("panda_arm")
+        self.get_logger().info("MoveItPy initialized and planning component loaded.")
+
         self.__action_server = ActionServer(
             self, Planning, "planning", self.execute_callback
         )
@@ -27,12 +39,20 @@ class PlanningActionServer(Node):
             base_url="https://generativelanguage.googleapis.com/v1beta/openai/",
         )
 
+        self.get_logger().info("PlanningActionServer initialized and ready.")
+
     def execute_callback(self, goal_handle: ServerGoalHandle):
         self.get_logger().info("Creating plan...")
 
+        self.planning_component.set_start_state(configuration_name="ready")
+        self.planning_component.set_goal_state(configuration_name="extended")
+
+        plan_result = self.planning_component.plan()
+        self.moveit_node.execute(plan_result.trajectory, blocking=True)
+
         prompt = goal_handle.request.prompt
 
-        response = self.client.chat.completions.create(
+        """response = self.client.chat.completions.create(
             model="gemini-2.5-flash",
             messages=[
                 {"role": "system", "content": "You are a robot."},
@@ -44,13 +64,13 @@ class PlanningActionServer(Node):
         for i in range(10):
             feedback_msg.incomplete_plan = f"waiting for vlm: {i}"
             goal_handle.publish_feedback(feedback_msg)
-            time.sleep(0.01)
+            time.sleep(0.01)"""
 
         result = Planning.Result()
-        result.plan = response.choices[0].message.content
+        # result.plan = response.choices[0].message.content
         goal_handle.succeed()
 
-        print(response.choices[0].message)
+        # print(response.choices[0].message)
 
         self.get_logger().info("Completed")
         return result
@@ -58,8 +78,30 @@ class PlanningActionServer(Node):
 
 def main(args=None):
     rclpy.init(args=args)
-    planning_action_server = PlanningActionServer()
-    rclpy.spin(planning_action_server)
+    moveit_config = (
+        MoveItConfigsBuilder(robot_name="panda", package_name="panda_moveit_config")
+        .robot_description(file_path="config/panda.urdf.xacro")
+        .planning_pipelines(pipelines=["ompl"])
+        .to_moveit_configs()
+        .to_dict()
+    )
+
+    moveit_py_node = MoveItPy(node_name="moveit_py", config_dict=moveit_config)
+
+    planning_action_server = PlanningActionServer(moveit_py_node)
+
+    executor = MultiThreadedExecutor()
+
+    executor.add_node(planning_action_server)
+
+    try:
+        executor.spin()
+    except KeyboardInterrupt:
+        pass
+    finally:
+        executor.shutdown()
+        planning_action_server.destroy_node()
+        rclpy.shutdown()
 
 
 if __name__ == "__main__":
